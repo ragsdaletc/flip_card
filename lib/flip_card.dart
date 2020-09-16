75c3a84141d96f78b7b91bdb24d9f81c6670202a
Ty Ragsdale <ragsdaletc@gmail.com>
2020-09-16 17:24:16 -0500

    Update flip_card.dart

--- lib/flip_card.dart
+++ lib/flip_card.dart
@@ -47,11 +47,12 @@ class AnimationCard extends StatelessWidget {
   
 typedef void BoolCallback(bool isFront);
 
+final soundList = ['sfx/card_shuffle0.mp3','sfx/card_shuffle1.mp3','sfx/card_shuffle2.mp3','sfx/card_shuffle3.mp3','sfx/card_shuffle4.mp3'];
+final AudioCache myPlayer = AudioCache();
+myPlayer.loadAll(List<String> soundList );
+
 class FlipCard extends StatefulWidget {
 
- final soundList = ['sfx/card_shuffle0.mp3','sfx/card_shuffle1.mp3','sfx/card_shuffle2.mp3','sfx/card_shuffle3.mp3','sfx/card_shuffle4.mp3'];
-  final AudioCache myPlayer = AudioCache();
- myPlayer.loadAll(List<String> soundList );
   final Widget front;
   final Widget back;
 
